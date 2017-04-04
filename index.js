f28300f93a90f7666a39df7f7d9a1067f499cedc
Benjamin Lupton <b@lupton.cc>
2017-04-04 08:22:20 +0800

    improved styling

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const m = window.m
 class DatabaseSetup {
 	view () {
 		return m('section',
-			m('span.button', 'Remote Database'),
+			m('span.action', 'Remote Database'),
 			' or ',
-			m('span.button', 'Local Database')
+			m('span.action', 'Local Database')
 		)
 	}
 }
+
 
 class PatreonSetup {
 	view () {
@@ -20,29 +21,26 @@ class PatreonSetup {
 			m('p', 'To fetch your patreon sponsors we need to crawl the patreon website. To do this, we need your patreon username and password.'),
 			m('input', { type: 'email', placeholder: 'your@patreon.email' }),
 			m('input', { type: 'password', placeholder: 'your patreon password' }),
-			m('span.button', 'Proceed →')
+			m('span.action', 'Proceed →')
 		)
 	}
 }
 
+class PatreonButton {
+	view () {
+		return m('span.action.patreon', 'Login with Patreon →')
+	}
+}
 class App {
 	view () {
 		return m('article',
-			m('h1', 'Welcome to Sponsored'),
-			m('ol.steps',
-				m('li',
-					m('section',
-						m('h2', 'Database'),
-						m(DatabaseSetup)
-					)
-				),
-				m('li.active',
-					m('section',
-						m('h2', 'Patreon'),
-						m(PatreonSetup)
-					)
-				),
-				m('li', m('h2', 'Open Collective'))
+			m('section.sidebar',
+				m('h1', 'Welcome to Sponsored'),
+			),
+			m('section.mainbar',
+				m(DatabaseSetup),
+				m(PatreonSetup),
+				m(PatreonButton)
 			)
 		)
 	}
