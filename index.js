1752c9b4ea1fbdbb6b7275e8631b9414e255ddad
Benjamin Lupton <b@lupton.cc>
2017-04-04 23:29:57 +0800

    styling improvements, submit button, tier display

--- index.js
+++ index.js
@@ -4,6 +4,28 @@
 
 const m = window.m
 
+class PatreonButton {
+	view () {
+		return m('span.action.patreon', 'Login with Patreon →')
+	}
+}
+
+class SubmitButton {
+	submit (e) {
+		const el = e.target
+		const submit =	el.parentNode.lastElementChild
+		submit.click()
+	}
+	view () {
+		return m('span',
+			m('.action.submit', { onclick: this.submit.bind(this) },
+				'Proceed →'
+			),
+			m('input.hide', { type: 'submit' })
+		)
+	}
+}
+
 class DatabaseSetup {
 	view () {
 		return m('section',
@@ -21,35 +43,106 @@ class PatreonSetup {
 			m('form',
 				m('input', { required: true, type: 'email', placeholder: 'your@patreon.email' }),
 				m('input', { required: true, type: 'password', placeholder: 'your patreon password' }),
-				m('input.action', { type: 'submit', value: 'Proceed →' })
+				m(SubmitButton)
 			)
 		)
 	}
 }
 
-class PatreonButton {
-	view () {
-		return m('span.action.patreon', 'Login with Patreon →')
+/*
+const object = Joi.object().keys({
+	email: Joi.email().required(),
+	name: Joi.string().required(),
+	description: Joi.string().label('tagline'),
+	website: Joi.string().uri().label('website url'),
+	logo: Joi.string().uri().label('logo url')
+})
+*/
+
+const fields = [
+	{ name: 'name', type: 'text', placeholder: 'name', required: true, autocomplete: 'name' },
+	{ name: 'description', type: 'text', placeholder: 'description' },
+	{ name: 'website', type: 'url', placeholder: 'website url', autocomplete: 'url' },
+	{ name: 'logo', type: 'url', placeholder: 'logo url' }
+]
+
+const tiers = [
+	{ cents: 500, name: 'Individual', description: 'Your sponsor listing includes a name' },
+	{ cents: 3000, name: 'Freelancer', description: 'Your sponsor listing includes a name and website' },
+	{ cents: 75000, name: 'Business', description: 'Your sponsor listing includes a name, website, and description' },
+	{ cents: 150000, name: 'Corporate', description: 'Your sponsor listing includes a name, website, description, and logo'}
+]
+
+function renderUserTier (m, user, tier) {
+	// $1500+
+	const logo = user.logo && tier.cents > 150000
+	// $750+
+	const description = user.slogan && tier.cents > 75000
+	// $30+
+	const website = user.website && tier.cents > 3000
+	// $5+
+	const name = user.name && tier.cents > 500
+	if (!name) return null
+
+	// Render
+	const title = description ? `${name}: ${description}` : name
+	if (logo) {
+		return website
+			? m('a', { href: website, target: '_blank', title },
+				m('img', { src: logo, alt: title })
+			)
+			: m('img', { src: logo, alt: title })
+	}
+	else {
+		return website
+			? m('span',
+				m('a', { href: website, target: '_blank', title },
+					name
+				),
+				description ? `: ${description}` : null
+			)
+			: m('span', title)
 	}
 }
 
-
-class AccountSetup {
+class UserSetup {
 	view () {
 		return m('section',
+			m('h2', 'User Details'),
 			m('form',
-				m('h2', 'Sponsor Details'),
-				m('input', { required: true, name: 'email', type: 'email', placeholder: 'email', autocomplete: 'email' }),
-				m('input', { required: true, name: 'name', type: 'text', placeholder: 'name', autocomplete: 'name' }),
-				m('input', { name: 'description', type: 'text', placeholder: 'tagline' }),
-				m('input', { name: 'website', type: 'url', placeholder: 'website url', autocomplete: 'url' }),
-				m('input', { name: 'logo', type: 'url', placeholder: 'logo url' }),
-				m('input.action', { type: 'submit', value: 'Proceed →' })
+				m('input', { name: 'email', type: 'email', placeholder: 'email', required: true, autocomplete: 'email' }),
+				m('input', { name: 'name', type: 'text', placeholder: 'name', required: true, autocomplete: 'name' }),
+				m(SubmitButton)
 			)
 		)
 	}
 }
 
+class SponsorSetup {
+	view () {
+		return m('section',
+			m('h2', 'Sponsor Details'),
+			m('form',
+				fields.map((attrs) => m('input', attrs)),
+				m(SubmitButton)
+			)
+		)
+	}
+}
+
+class TierSetup {
+	view () {
+		return m('section',
+			m('h2', 'Tier Selection'),
+			m('form',
+				m('select', { name: 'tier', required: true },
+					tiers.map((tier) => m('option', { value: tier.cents, title: tier.description }, tier.name))
+				),
+				m(SubmitButton)
+			)
+		)
+	}
+}
 
 class App {
 	view () {
@@ -61,7 +154,9 @@ class App {
 				m(DatabaseSetup),
 				m(PatreonSetup),
 				m(PatreonButton),
-				m(AccountSetup)
+				m(UserSetup),
+				m(SponsorSetup),
+				m(TierSetup)
 			)
 		)
 	}
